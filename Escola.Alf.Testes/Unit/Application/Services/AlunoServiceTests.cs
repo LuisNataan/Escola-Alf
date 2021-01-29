14f0d0c2b535e1b9bb34af5318c77bae154ce9ee
LuisNataan <luisnataan556@gmail.com>
2021-01-29 19:22:20 -0300

    Criação dos Testes de AlunoService

---
+++ Escola.Alf.Testes/Unit/Application/Services/AlunoServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Escola.Alf.Application.Interfaces;
+using Escola.Alf.Application.Model.Aluno;
+using Escola.Alf.Application.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Escola.Alf.Testes.Unit.Application.Services
+{
+    public class AlunoServiceTests
+    {
+        private readonly IAlunoRepository _alunoRepository;
+        private readonly AlunoService _alunoService;
+
+        public AlunoServiceTests()
+        {
+            _alunoRepository = NSubstitute.Substitute.For<IAlunoRepository>();
+            _alunoService = new AlunoService(_alunoRepository);
+        }
+
+        [Fact]
+        public async Task Create_DeveCriarAluno()
+        {
+            var alunoRequestModel = new AlunoRequestModel()
+            {
+                Nome = "João da Silva",
+                Email = "jaozinho@gmail.com",
+                DataNascimento = "02/10/2001",
+            };
+
+        }
+    }
+}
